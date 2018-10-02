5fd5a7902131ef3ecf896006b0b9bef26cc1e97f
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-10-02 20:23:42 +0900

    docs: コードを理解しやすくするため

--- src/main/scala/interfaces/Paper.scala
+++ src/main/scala/interfaces/Paper.scala
@@ -6,21 +6,31 @@ import domain.paper.{List, ListContinue}
 import io.circe._
 import io.circe.generic.semiauto._
 
+/**
+  * Dropbox Paper に関する処理を行う。
+  *
+  * レスポンスに関しては、
+  * 型を指定して実行すると「polymorphic expression cannot be instantiated to expected type;」というエラーが発生するため
+  * 一度変数に格納している。
+  * よくわかっていないが、おそらく https://github.com/scala/bug/issues/8983 に関係するものだと思われる。
+  *
+  * @param token Dropbox Paper へアクセスする際に使用するトークン
+  */
 class Paper(val token: String) {
   implicit val backend: SttpBackend[Id, Nothing] = HttpURLConnectionBackend()
 
+  /**
+    * ドキュメントの一覧を取得する。
+    *
+    * @return 取得した一覧
+    */
   def list(): Response[Either[DeserializationError[Error], List.Response]] = {
-
     implicit val encoder: Encoder[List.Parameter] = deriveEncoder[List.Parameter].mapJsonObject(_.filter {
       case (_, Json.Null) => false
       case _              => true
     })
     implicit val cursorDecoder: Decoder[List.Cursor] = deriveDecoder[List.Cursor]
     implicit val decoder: Decoder[List.Response] = deriveDecoder[List.Response]
-
-    // 型を指定して実行すると「polymorphic expression cannot be instantiated to expected type;」が発生する
-    // おそらくこれ？ -> https://github.com/scala/bug/issues/8983
-    // そのため一度変数に格納する
     val r = sttp
       .post(uri"https://api.dropboxapi.com/2/paper/docs/list")
       .auth
@@ -32,6 +42,12 @@ class Paper(val token: String) {
     r
   }
 
+  /**
+    * ドキュメントの一覧の続きを取得する。
+    *
+    * @param cursor この値に指定した続きから取得する。
+    * @return 取得した一覧
+    */
   def listContinue(cursor: String) = {
     implicit val encoder: Encoder[ListContinue.Parameter] = deriveEncoder
     val r = sttp
